6d20e099763df56f2403db0a9b1fedf3a3914e11
Alessandro C. <Alessandromaria.calista@gmail.com>
2026-08-21 20:24:24 +0200

    fix(resources): imporved resources domain and removed old implementation.

--- src/hoarder/resource/domain/resource.py
+++ src/hoarder/resource/domain/resource.py
@@ -1,23 +1,38 @@
-from dataclasses import dataclass
-from datetime import datetime
+from sqlmodel import SQLModel, Field, Relationship
 from enum import Enum
 
-class ResourceType(Enum):
-    FILE = "file",
-    LINK = "link",
+from typing import TYPE_CHECKING
 
-@dataclass
-class Resource:
+if TYPE_CHECKING:
+    from hoarder.course.domain.course import UniversityCourse
+
+
+class ResourceType(str, Enum):
+    FILE = "file"
+    LINK = "link"
+
+class Resource(SQLModel, table = True):
+    __tablename__ = "resources"
+    id: int | None = Field(primary_key=True, index=True, default=None)
+    name: str = Field(nullable=False, index=True)
+    author: str = Field(nullable=False)
+
+    type: ResourceType
+    path: str | None = Field(default=None)
+    url: str | None = Field(default=None)
+
+    course_id: int = Field(foreign_key="university_courses.id")
+
+    course: "UniversityCourse" = Relationship(back_populates="resources")
+
+
+class ResourcePublic(SQLModel, table = False):
     id: int
     name: str
-    creation_date: datetime
-    user_id_added_by: int
-    tags: list[str]
+    author: str
 
-@dataclass
-class FileSystemResource(Resource):
-    fs_path: str
-
-@dataclass
-class LinkResource(Resource):
-    link: str
+    type: ResourceType
+    path: str | None = None
+    url: str | None = None
+    
+    course_id: int
