0506d501246b9b581cdefc81bbc79e72787add8e
Alessandro C. <Alessandromaria.calista@gmail.com>
2026-08-27 23:12:23 +0200

    feat(course): new patch endpoint to update courses

--- src/hoarder/course/application/service.py
+++ src/hoarder/course/application/service.py
@@ -1,5 +1,5 @@
-from sqlmodel import Session, select, insert, delete
-from hoarder.course.domain.course import UniversityCourse, CourseCreate
+from sqlmodel import Session, select, insert, delete, update
+from hoarder.course.domain.course import UniversityCourse, CourseCreate, CourseUpdate
 
 def get_all_courses(session: Session) -> list[UniversityCourse]:
     return session.exec(select(UniversityCourse)).all()
@@ -15,3 +15,11 @@ def delete_course(session: Session, id: int):
     statement = delete(UniversityCourse).where(UniversityCourse.id == id)
     session.exec(statement)
     session.commit()
+
+def update_course(session: Session, id: int, update_payload: CourseUpdate):
+    statement = select(UniversityCourse).where(UniversityCourse.id == id)
+    course : UniversityCourse = session.exec(statement).first()
+    if course:
+        course.name = update_payload.name
+        course.description = update_payload.description
+        course.semester = update_payload.semester
--- src/hoarder/course/routes.py
+++ src/hoarder/course/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Response
 from sqlmodel import Session
 
 import hoarder.course.application.service as service
-from hoarder.course.domain.course import CoursePublic, CourseCreate 
+from hoarder.course.domain.course import CoursePublic, CourseCreate, CourseUpdate
 from hoarder.core.db import get_session
 
 course = APIRouter(prefix="/course", tags=["university_course"])
@@ -11,10 +11,13 @@ course = APIRouter(prefix="/course", tags=["university_course"])
 async def get_all_courses(session: Session = Depends(get_session)):
     return service.get_all_courses(session)
 
-
 @course.post("/", response_model=CoursePublic)
 async def create_course(create_payload: CourseCreate, session: Session = Depends(get_session)):
     return service.create_course(session, create_payload)
+
+@course.patch("/{id}")
+async def update_course(id: int, update_payload: CourseUpdate, session: Session = Depends(get_session)):
+    return service.update_course(session, id, update_payload)
 
 @course.delete("/{id}")
 async def delete_course(id: int, session: Session = Depends(get_session)):
